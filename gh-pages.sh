bc33e6024b97fc5265744a7ee86976050186e238
diamondburned <datutbrus@gmail.com>
2018-08-09 19:17:47 +0700

    alifhdf

--- gh-pages.sh
+++ gh-pages.sh
@@ -3,7 +3,7 @@
 if [[ $1 = "pull" ]]; then
 	git subtree pull --prefix public origin gh-pages
 elif [[ $1 = "force" ]]; then
-	git push origin $(git subtree split --prefix public dist master):gh-pages --force
+	git push origin $(git subtree split --prefix public master):gh-pages --force
 else
 	git subtree push --prefix public origin gh-pages
 fi
