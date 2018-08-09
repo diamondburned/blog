02e37dfed4b27f14cebd9759c33f876142373bfe
diamondburned <datutbrus@gmail.com>
2018-08-09 19:37:25 +0700

    guyeen7ohFiem5uniech2ahcaeghuneezi1eeRuweetitie2mahsh2Ohp9jaeZiev2nah2ohze0eir0obai9niloo3thee8nae3I

--- gh-pages.sh
+++ gh-pages.sh
@@ -4,7 +4,12 @@ if [[ $1 = "pull" ]]; then
 	git subtree pull --prefix public origin gh-pages
 elif [[ $1 = "force" ]]; then
 	git push origin $(git subtree split --prefix public master):gh-pages --force
+elif [[ $1 = "push" ]]; then
+	git subtree push --prefix public origin gh-pages
 else
+	git add -A
+	git commit -m "$(pwgen 100 1)"
+	git push origin
 	git subtree push --prefix public origin gh-pages
 fi
 #git push -f master gh-pages
